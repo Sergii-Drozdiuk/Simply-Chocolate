5d837468aa901893e98709066aa9fee828b867c4
Valentun2 <pocomaxawrkt@gmail.com>
2023-06-04 12:14:35 +0300

    js

---
+++ js/reviews.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 16,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+      margin: 28,
+    },
+  },
+});
