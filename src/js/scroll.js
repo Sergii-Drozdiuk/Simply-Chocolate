c97c19c13a90ddc8f92f02c84a66660b0f52c35f
chertik77 <babychdenya@gmail.com>
2023-06-10 10:23:01 +0100

    update

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -63,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (key == 27) {
         document.querySelector('.modal.active').classList.remove('active');
         document.querySelector('.overlay').classList.remove('active');
+        document.body.classList.remove('active');
       }
     },
     false
@@ -71,5 +72,6 @@ document.addEventListener('DOMContentLoaded', function () {
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
+    document.body.classList.remove('active');
   });
 });
